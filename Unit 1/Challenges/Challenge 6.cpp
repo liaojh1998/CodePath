60c38033900d5dc952ee717e97710b828160f86b
BuildTools <liaojh98@gmail.com>
2016-10-27 01:08:33 -0500

    10/27/2016
    Modified Challenge 6 to be more perceptible to empty strings

--- Unit 1/Challenges/Challenge 6.cpp
+++ Unit 1/Challenges/Challenge 6.cpp
@@ -1,41 +1,31 @@
-#include <cstdio>
+#include <iostream>
+#include <sstream>
+#include <string>
 #include <cstring>
-int readStr(char *str){
-	register char c = getchar();
-	register int len = 0;
-	if(!(~c)) return -1;
-	while(c < 33 && ~c) c = getchar(); //32 if space should be included
-	//32 if stop at space, 10 if stop at next line
-	while(c != 10 && c != 32 && ~c){
-		str[len] = c;
-		c = getchar();
-		len++;
-	}
-	str[len] = '\0';
-	//Just in case if needs to return arriving at end of line
-	//if(c == 10) return 10;
-	return 1;
-}
+using namespace std;
 int main(){
+	//Just to speed things up
+	cin.tie(0);
+	ios_base::sync_with_stdio(0);
+	string s;
+	getline(cin, s);
+	stringstream ss;
+	ss << s;
 	int N;
-	scanf("%d", &N);
-	getchar();
+	ss >> N;
 	//Assume N <= 1000, |str| <= 1000
-	char str[1000][1000];
+	string str[1000];
 	for(int i = 0; i < N; i++)
-		readStr(str[i]);
+		getline(cin, str[i]);
 	bool dp[1000][1000];
 	memset(dp[0], 1, sizeof(dp[0]));
 	for(int i = 1; i < N; i++){
-		int len = strlen(str[i]);
+		int len = str[i].size();
 		for(int j = 0; j < len && dp[i-1][j] && str[i][j] == str[i-1][j]; j++)
 			dp[i][j] = true;
 	}
-	int len = strlen(str[N-1]), end;
+	int len = str[N-1].size(), end;
 	for(end = 0; end < len && dp[N-1][end]; end++);
-	char ans[1000];
-	memset(ans, 0, sizeof(ans));
-	memcpy(ans, str[N-1], end);
-	printf("%s\n", ans);
+	cout << str[N-1].substr(0, end) << endl;
 	return 0;
 }
