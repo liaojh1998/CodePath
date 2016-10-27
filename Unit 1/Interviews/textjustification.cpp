27c6fba9844a7df7098c4f10c22b46d4370f8b8d
BuildTools <liaojh98@gmail.com>
2016-10-27 16:34:26 -0500

    10/27/2016
    Finished Interview Question 2

--- Unit 1/Interviews/textjustification.cpp
+++ Unit 1/Interviews/textjustification.cpp
@@ -3,7 +3,73 @@
 #include <string>
 #include <cstring>
 #include <vector>
+#include <queue>
 using namespace std;
+string space(int len){
+	char str[len];
+	memset(str, ' ', sizeof(str));
+	str[len] = '\0';
+	string s(str);
+	return s;
+}
+void justtext(string str, int justlen, vector<string> &ans){
+	stringstream ss;
+	ss << str;
+	queue<string> q;
+	string next;
+	while(ss.good() || !q.empty()){
+		int len = q.empty() ? 0 : q.front().size();
+		int num = q.empty() ? 0 : 1;
+		while(ss.good()){
+			ss >> next;
+			q.push(next);
+			if(len + next.size() + num - 1 < justlen){
+				num++;
+				len+= next.size();
+			}else{
+				break;
+			}
+		}
+		//This is based on the assumption that word length will always be <= justification length
+		if(num == 1){
+			int size = justlen-len;
+			string shorter = space(size);
+			string answer;
+			answer += q.front();
+			q.pop();
+			ans.push_back(answer);
+		}else if((justlen-len)%(num-1)){
+			int left = (justlen-len)%(num-1);
+			int size = (justlen-len)/(num-1);
+			string longer = space(size+1);
+			string shorter = space(size);
+			string answer;
+			for(int i = 0; i < num-1; i++){
+				answer += q.front();
+				q.pop();
+				if(i < left)
+					answer += longer;
+				else
+					answer += shorter;
+			}
+			answer += q.front();
+			q.pop();
+			ans.push_back(answer);
+		}else{
+			int size = (justlen-len)/(num-1);
+			string shorter = space(size);
+			string answer;
+			for(int i = 0; i < num-1; i++){
+				answer += q.front();
+				q.pop();
+				answer += shorter;
+			}
+			answer += q.front();
+			q.pop();
+			ans.push_back(answer);
+		}
+	}
+}
 int main(){
 	//Pretty interesting but annoying implementation problem
 	cin.tie(NULL);
@@ -20,5 +86,9 @@ int main(){
 	//Text Justification
 	vector<string> ans;
 	justtext(str, justlen, ans);
+	//Print
+	int size = ans.size();
+	for(int i = 0; i < size; i++)
+		cout << ans[i] << endl;
 	return 0;
 }
